8a2a448b59451f26758c71de45a3305ea971e04a
Iryna Ishchanka <28myhome@gmail.com>
2022-06-24 22:42:52 +0300

    fet: write function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
   
+    let numString = String(number);
+            let arrOne = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+                        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+            let arrTwo = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+            if (number === 0) {
+                return 'zero'
+
+            } else if (number < 20) {
+                return arrOne[number]
+
+            } else if (numString.length === 2) {
+                let firstNum = +numString[0];
+                let lastNum = +numString[1];
+                return `${arrTwo[firstNum-2]} ${arrOne[lastNum]}`;
+
+            } else {
+                let firstNum = +numString[0];
+                let lastsNumbers = +numString.slice(1);
+                let midleNum = +numString[1];
+                let lastNum = +numString[2];
+
+                if (lasNum === 0) {
+                    return `${arrOne[firstNum]} hundred`;
+
+                } else if (lastsNumbers < 20) {
+                    return `${arrOne[firstNum]} hundred ${arrOne[lastsNumbers]}`;
+
+                } else {
+                    return `${arrOne[firstNum]} hundred ${arrTwo[midleNum - 2]} ${arrOne[lastNum]}`
+                }
+            }
+
+
+    
 }
